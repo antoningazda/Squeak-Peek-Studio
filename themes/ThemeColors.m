5b1b3fa5850647f1759526139521c2796fbd645b
Gazda, Antonín <35578312+antoningazda@users.noreply.github.com>
2025-05-02 08:58:43 +0200

    Generate new default config, Edit colors

--- themes/ThemeColors.m
+++ themes/ThemeColors.m
@@ -1,7 +1,13 @@
 classdef ThemeColors
     properties (Constant)
-        Light = struct("Background", [1 1 1], "Text", [0 0 0], "Primary", [0.2 0.6 1], "Accent", [1 0.5 0]);
-        Dark  = struct("Background", [0.1 0.1 0.1], "Text", [1 1 1], "Primary", [0.3 0.7 0.3], "Accent", [1 0.2 0.2]);
+Light = struct( ...
+    "Background", [1, 1, 1], ...            % pure white
+    "Text", [0.05, 0.05, 0.05], ...         % near-black text
+    "Primary", [125/255, 206/255, 210/255], ...        % brighter soft blue
+    "Accent", [249/255, 192/255, 110/255] ...          % brighter orange-yellow
+);
+
+Dark  = struct("Background", [0.1 0.1 0.1], "Text", [1 1 1], "Primary", [0.3 0.7 0.3], "Accent", [1 0.2 0.2]);
     end
 
     methods (Static)
